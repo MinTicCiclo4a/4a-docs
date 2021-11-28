3f09d5b618fea5828a167239784f44bbe26f7cda
reybercaceres <reyberCaceres@gmail.com>
2021-11-27 22:30:07 -0300

    se agrega Microservicio controcitas

---
+++ controlcitas_ms/src/main/java/com/misiontic/controlcitas_ms/ControlcitasMsApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.controlcitas_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlcitasMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlcitasMsApplication.class, args);
+	}
+
+}
---
+++ controlcitas_ms/src/test/java/com/misiontic/controlcitas_ms/ControlcitasMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.controlcitas_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlcitasMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
